e7beacf47b5cbf5280bf9c6b2e19640472bdab37
atharv890 <85354808+atharv890@users.noreply.github.com>
2022-02-20 13:06:24 +0530

    Add files via upload

---
+++ main.1.js
@@ -0,0 +1,19 @@
+function hello(){
+no1=Math.random()*99;
+no2=Math.random()*99;
+document.getElementById("number1").innerHTML=no1;
+document.getElementById("number2").innerHTML=no2;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,12 @@
+function submit(){
+    window.location="Addition.html";
+}
+function submit1(){
+    window.location="Subtraction.html";
+}
+function submit2(){
+    window.location="Multiplication.html";
+}
+function submit3(){
+    window.location="Division.html";
+}
